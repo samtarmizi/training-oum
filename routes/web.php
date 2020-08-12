9a452c01402e204e9afae54a97a54ebad0bcd7ec
Tarmizi Sanusi <realtarmizisanusi@gmail.com>
2020-08-12 12:43:10 +0800

    Add authentication

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Closure - cannot cache routes
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
